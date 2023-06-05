2942a159b0f83e9c869f92ec35dac53c8603f75c
melus7n7 <sulem477@gmail.com>
2023-06-05 01:32:43 -0600

    Vista trabajos recepcionales

---
+++ src/javafxsspger/controladores/FXMLTrabajosRecepcionalesController.java
@@ -0,0 +1,22 @@
+/*
+*Autor: Martínez Aguilar Sulem, Montiel Salas Jesús Jacob
+*Fecha de creación: 05/06/2023
+*Fecha de modificación: 05/06/2023
+*Descripción: Controlador de la vista de los trabajos recepcionales
+*/
+package javafxsspger.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLTrabajosRecepcionalesController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
