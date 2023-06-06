bf9a095293f0e14bca71788b7384bb73b7fbd112
melus7n7 <sulem477@gmail.com>
2023-06-06 05:19:45 -0600

    Modificacion

---
+++ src/javafxsspger/controladores/FXMLModificacionTrabajoRecepcionalController.java
@@ -0,0 +1,28 @@
+/*
+*Autor: Martínez Aguilar Sulem
+*Fecha de creación: 06/06/2023
+*Fecha de modificación: 06/06/2023
+*Descripción: Controlador de la vista de la modificacion de un trabajo recepcional
+*/
+package javafxsspger.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author sulem
+ */
+public class FXMLModificacionTrabajoRecepcionalController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
